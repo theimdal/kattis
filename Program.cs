5ac2e1eecf7e1bd52b614f508752ce5f202e76d7
Tord Heimdal <toh@dips.no>
2022-01-25 11:59:44 +0100

    solved RadioCommercials.

--- Program.cs
+++ Program.cs
@@ -3,47 +3,113 @@ using System.Collections.Generic;
 
 namespace kattis
 {
-    class  OddManOut {
+    class  RadioCommercials {
         static void Main(string[] args)
         {
-            var numTestCases = Int32.Parse(Console.ReadLine());
-            
-            for(int i = 1; i <= numTestCases; i++)
+
+            (int numberOfCommercials, int priceOfOneCommercial) firstInput = handleFirstInputLine();
+            int[] arrayOfStudentsListeningPerBreak = handleSecondInputLine();
+            int[] arrayOfNetIncomePerBreak = calculateNetIncomePerBreak(firstInput.priceOfOneCommercial, arrayOfStudentsListeningPerBreak);
+            Console.WriteLine(maxSumSubSequence(arrayOfNetIncomePerBreak).ToString());
+        }
+
+        static int[] calculateNetIncomePerBreak(int priceOfOneCommercial, int[] arrayOfStudentsListeningPerBreak)
+        {
+            for(int i = 0; i < arrayOfStudentsListeningPerBreak.Length; i++)
             {
-                processCase(i);
+                int currentValue = arrayOfStudentsListeningPerBreak[i];
+                arrayOfStudentsListeningPerBreak[i] = currentValue - priceOfOneCommercial;
             }
+
+            return  arrayOfStudentsListeningPerBreak;
         }
 
-        static void processCase(int caseNumber)
-        {
-            var numberOfGuests = Int32.Parse(Console.ReadLine());
+        static int maxSumSubSequence(int[] array) 
+        {            
 
-            if(numberOfGuests % 2 == 0)
-                return;
+            int maxSum = Int32.MinValue;
+            int startIndexFinal = 0;
+            int endIndexFinal = 0;
+            int currentSum = 0;
+            int startIndexCurrent = 0;
 
-            findUniqueCode(Console.ReadLine().Split(" "), caseNumber);
-        }
-
-        static void findUniqueCode(String[] guestCodes, int caseNumber)
-        {
-            Dictionary<long, int> map = new Dictionary<long, int>();
-            for (var i = 0; i< guestCodes.Length; i++)
-            {                                
-                long key = long.Parse(guestCodes[i]);
-
-                if(map.ContainsKey(key))
+            for (int i = 0; i < array.Length; i++)
+            {
+                currentSum += array[i];
+                if(currentSum > maxSum) 
                 {
-                    map[key]++;
+                    maxSum = currentSum;
+                    endIndexFinal = i;
+                    startIndexFinal = startIndexCurrent;
                 }
-                else
-                    map.Add(key, 1);
+                if(currentSum <= 0)
+                {
+                    currentSum = 0;
+                    startIndexCurrent = i + 1;
+                }
+            }            
+            return maxSum;
+        }
+
+
+        static (int numberOfCommercials, int priceOfOneCommercial) handleFirstInputLine()
+        {
+            int numberOfCommercials = 0;
+            int priceOfOneCommercial = 0;
+
+            try {
+                    var input = Console.ReadLine().Split(" ");
+
+                    if(input != null)
+                    {
+                        Int32.TryParse(input[0], out numberOfCommercials);
+                        Int32.TryParse(input[1], out priceOfOneCommercial);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Input is missing. Stopping program.");
+                        Environment.Exit(-1);   
+                    }
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine("Error parsing input. Stopping program.");
+                Environment.Exit(-1);
             }
 
-            foreach(KeyValuePair<long, int> entry in map)
+            return (numberOfCommercials, priceOfOneCommercial);
+        }
+
+        static int[] handleSecondInputLine()
+        {
+            try 
             {
-                if(entry.Value == 1)
-                    Console.WriteLine($"Case #{caseNumber}: {entry.Key}");
+                    var input = Console.ReadLine().Split(" ");
+                    
+                    int[] arrayOfStudentsListeningPerBreak = new int[input.Length];
+
+                    if(input != null)
+                    {
+                        for (int i = 0; i < input.Length; i++) 
+                        {
+                            Int32.TryParse(input[i], out arrayOfStudentsListeningPerBreak[i]);
+                        }                        
+                    } 
+                    else
+                    {
+                        Console.WriteLine("Input is missing. Stopping program.");
+                        Environment.Exit(-1);   
+                    }
+
+                    return arrayOfStudentsListeningPerBreak;
             }
+            catch(Exception err)
+            {
+                Console.WriteLine("Error parsing input. Stopping program.");
+                Environment.Exit(-1);
+            }
+
+            return null;
         }
     }
 }
